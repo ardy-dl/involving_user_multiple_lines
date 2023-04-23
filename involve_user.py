b92c1d3b03c33220f9a442db3de9b1749cef6810
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-04-23 11:39:10 +0800

    tested if the file is working

--- involve_user.py
+++ involve_user.py
@@ -1,6 +1,8 @@
-# Write a method in python to write multiple line of text contents into a text file mylife.txt.
+# Write a method in python to write multiple line of text contents into a text file.
 
-# open mylife.txt(append)
+# open myfriends.txt(append)
+with open("my_friends.txt", "a") as life_friends:
+    life_friends.write("abcdsefsv")
 # ask for input
 # ask if there's more to write
 # write the input in the file
