6688a60ebc1a693aaaa5580138d418049aa64649
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-04-23 11:57:16 +0800

    Code working with no errors

--- involve_user.py
+++ involve_user.py
@@ -4,10 +4,10 @@
 with open("my_friends.txt", "a") as life_friends:
     add = "yes"
     # loop to keep adding next 
-    while add == "yes":
+    while add.lower() == "yes":
         # ask for input
         friends = input("Enter a name of your friend: ")
         # write the input in the file
-        life_friends.write(friends)
+        life_friends.write(friends + "\n")
         # ask if there's more
         add = input("Are there more lines? ")
