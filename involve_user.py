f3f9771bfe744ea4a6d797a305a89cf8b0f4305f
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-04-23 11:33:18 +0800

    pseudocode done

---
+++ involve_user.py
@@ -0,0 +1,6 @@
+# Write a method in python to write multiple line of text contents into a text file mylife.txt.
+
+# open mylife.txt(append)
+# ask for input
+# ask if there's more to write
+# write the input in the file
